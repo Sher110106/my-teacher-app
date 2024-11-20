8ea064055fa009b3b2b49de9c751b850b83a283d
Sher110106 <sher.singh.ug23@plaksha.edu.in>
2024-11-20 23:35:07 +0530

    Email confirmation changes

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -1,4 +1,4 @@
-import { resetPasswordAction } from "@/app/actions";
+import { forgotPasswordAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
@@ -28,7 +28,7 @@ export default async function ResetPassword(props: {
         placeholder="Confirm password"
         required
       />
-      <SubmitButton formAction={resetPasswordAction}>
+      <SubmitButton formAction={forgotPasswordAction}>
         Reset password
       </SubmitButton>
       <FormMessage message={searchParams} />
